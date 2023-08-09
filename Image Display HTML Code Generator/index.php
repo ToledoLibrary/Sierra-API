ba62cd7cd9c5a0a21df575c89683aea1d3490aa6
Roxanna Foster <47989102+roxannafoster@users.noreply.github.com>
2023-08-09 09:07:03 -0400

    Update fix typos

--- Image Display HTML Code Generator/index.php
+++ Image Display HTML Code Generator/index.php
@@ -6,7 +6,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Book Tools</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="XXXX" crossorigin="anonymous">
     <link rel="stylesheet" href="imagestyles.css">
   </head>
   <body>
